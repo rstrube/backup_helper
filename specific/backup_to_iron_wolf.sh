4942123f494370f660a46cbd401a2097da5e1075
Robert <rstrube@gmail.com>
2025-03-09 22:25:28 -0600

    Fixed some pathing issues

--- specific/backup_to_iron_wolf.sh
+++ specific/backup_to_iron_wolf.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-../../helper/rsync/rsync_helper.sh -v -d -l ~/Documents ~/Downloads ~/Music ~/Pictures ~/Projects ~/Games ~/Music /run/media/robert/Iron_Wolf
+../helper/rsync/rsync_helper.sh -v -d -l ~/Documents ~/Downloads ~/Music ~/Pictures ~/Projects ~/Games ~/Music /run/media/robert/Iron_Wolf
--- specific/backup_to_nas.sh
+++ specific/backup_to_nas.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-../../helper/rsync/rsync_helper.sh -v -d -s ~/Music robert@192.168.1.200:/mnt/main/media
+../helper/rsync/rsync_helper.sh -v -d -s ~/Music robert@192.168.1.200:/mnt/main/media
 
-../../helper/rsync/rsync_helper.sh -v -d -s ~/Documents robert@192.168.1.200:/mnt/main/personal/robert
+../helper/rsync/rsync_helper.sh -v -d -s ~/Documents robert@192.168.1.200:/mnt/main/personal/robert
 
-../../helper/rsync/rsync_helper.sh -v -d -s ~/Projects robert@192.168.1.200:/mnt/main/personal/robert
+../helper/rsync/rsync_helper.sh -v -d -s ~/Projects robert@192.168.1.200:/mnt/main/personal/robert
 
-../../helper/rsync/rsync_helper.sh -v -d -s ~/Games robert@192.168.1.200:/mnt/main/personal/robert
+../helper/rsync/rsync_helper.sh -v -d -s ~/Games robert@192.168.1.200:/mnt/main/personal/robert
