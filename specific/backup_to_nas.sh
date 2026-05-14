2601cbcf2d0bbfa5b80ebbd8636918d03af6f9df
Robert Strube <rstrube@gmail.com>
2026-05-14 16:58:25 -0600

    Simplify backup_to_nas.sh by combining sources with a shared destination
    Documents, Projects, and Games all target /mnt/main/personal/robert
    so they can be passed as multiple sources in a single rsync_helper call.

--- specific/backup_to_nas.sh
+++ specific/backup_to_nas.sh
@@ -2,8 +2,4 @@
 
 ../helper/rsync/rsync_helper.sh -v -d ~/Music robert@truenas:/mnt/main/media
 
-../helper/rsync/rsync_helper.sh -v -d ~/Documents robert@truenas:/mnt/main/personal/robert
-
-../helper/rsync/rsync_helper.sh -v -d ~/Projects robert@truenas:/mnt/main/personal/robert
-
-../helper/rsync/rsync_helper.sh -v -d ~/Games robert@truenas:/mnt/main/personal/robert
+../helper/rsync/rsync_helper.sh -v -d ~/Documents ~/Projects ~/Games robert@truenas:/mnt/main/personal/robert
